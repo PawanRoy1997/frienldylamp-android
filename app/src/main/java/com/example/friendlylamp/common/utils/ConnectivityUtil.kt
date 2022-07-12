f8873881e7019adeaf65c2c54f20b5f08cb84697
pawanroy <proy@teckborn.com>
2022-07-12 20:30:02 +0530

    Network State permission and ConnectivityUtil.kt

---
+++ app/src/main/java/com/example/friendlylamp/common/utils/ConnectivityUtil.kt
@@ -0,0 +1,30 @@
+package com.example.friendlylamp.common.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class ConnectivityUtil {
+    /**
+     * To check if devices has an active internet connection or not.
+     *
+     * true -> has Internet connection
+     * false -> is offline
+     *
+     * @author Pawan Roy
+     * @since 1.0
+     * @param context of type [Context]
+     * @return result of type [Boolean]
+     */
+    fun isConnected(context: Context): Boolean {
+        return context.connectivityManager().hasInternet()
+    }
+
+    private fun Context.connectivityManager() =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private fun ConnectivityManager.hasInternet(): Boolean {
+        return this.getNetworkCapabilities(this.activeNetwork)
+            ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+    }
+}
